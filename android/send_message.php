49069b8c0df848e70171aaea7d442fd2245e0f4d
Manish Kumar Pathak <pathakmanish86@gmail.com>
2020-02-07 10:28:12 +0530

    Update send_message.php

--- android/send_message.php
+++ android/send_message.php
@@ -1,7 +1,7 @@
 <?php
 include("../config/config.php");
-$regId = "cbFW-BZwE4I:APA91bGpQMAPq4i4W5eSW0vgZghPpCWWifpU29cEp71G8COBioKDzDwvwlNSAUtu1d1mnl0livwnvxbFCA2JGZfFcUbL53yBcRXojAWPoPlWhdGO8m0GZ1gu6e7K8T3LWthzwS2yFkHU";
-//$regId ="flI4EbgWrkU:APA91bHMTaeGeeZlfY-RUcKxTpP1yqixjZYAk6reKTVdl43A5hgpw_OxU0NwjxkkPUdNEA4gC6KzHyfwkCLEOOxT92mbla8mPGLQ1-0glkRthmkwm_kud-Fgep_vYrf4R6CqAbrMDIWD";
+$regId = "";
+//$regId ="";
 $message = "Hello This is the test Message";
 include_once('gcm.php');
 $gcm = new GCM();
